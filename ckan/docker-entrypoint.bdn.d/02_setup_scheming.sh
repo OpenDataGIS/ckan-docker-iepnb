2621d275470f4feaf836bc3d572d2bf16a9b48f5
mjanez <96422458+mjanez@users.noreply.github.com>
2024-03-21 17:24:32 +0000

    Update ckanext-schemingdcat v1.2.2
    - Update ckanext-dcat v1.6.0
    - Added workers
    - Improve images

--- ckan/docker-entrypoint.bdn.d/02_setup_scheming.sh
+++ ckan/docker-entrypoint.bdn.d/02_setup_scheming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Update ckanext-scheming and ckanext-scheming_dcat settings defined in the env var
-echo "Loading ckanext-scheming and ckanext-scheming_dcat settings into ckan.ini"
+# Update ckanext-scheming and ckanext-schemingdcat settings defined in the env var
+echo "Loading ckanext-scheming and ckanext-schemingdcat settings into ckan.ini"
 ckan config-tool $CKAN_INI \
-    "scheming.dataset_schemas=$CKANEXT__SCHEMING_DCAT_DATASET_SCHEMA" \
-    "scheming.presets=$CKANEXT__SCHEMING_DCAT_PRESETS" 
+    "scheming.dataset_schemas=$CKANEXT__SCHEMINGDCAT_DATASET_SCHEMA" \
+    "scheming.presets=$CKANEXT__SCHEMINGDCAT_PRESETS" 
