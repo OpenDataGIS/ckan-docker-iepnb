6aa04b6f01cdb509f54122004d5f0729d5730cb2
mjanez <96422458+mjanez@users.noreply.github.com>
2024-05-28 13:03:37 +0200

    Update CKAN configuration files with Docker entrypoint changes
    - Update extensions: ckanext-iepnb 2.1.0, ckanext-schemingdcat v3.1.0 and ckanext-sparql_interface 2.0.3-iepnb

--- ckan/docker-entrypoint.d/02_setup_scheming.sh
+++ ckan/docker-entrypoint.d/02_setup_scheming.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update ckanext-scheming and ckanext-schemingdcat settings defined in the env var
-echo "Set up ckanext-schemingdcat. Clear index"
+echo "[docker-entrypoint.02_setup_scheming] Clear index"
 ckan -c $CKAN_INI search-index clear
 
-echo "Loading ckanext-scheming and ckanext-schemingdcat settings into ckan.ini"
+echo "[docker-entrypoint.02_setup_scheming] Loading ckanext-scheming and ckanext-schemingdcat settings into ckan.ini"
 ckan config-tool $CKAN_INI \
     "scheming.dataset_schemas=$CKANEXT__SCHEMINGDCAT_DATASET_SCHEMA" \
     "scheming.group_schemas=$CKANEXT__SCHEMINGDCAT_GROUP_SCHEMAS" \
@@ -12,7 +12,10 @@ ckan config-tool $CKAN_INI \
     "scheming.presets=$CKANEXT__SCHEMINGDCAT_PRESETS" \
     "schemingdcat.facet_list=$CKANEXT__SCHEMINGDCAT_FACET_LIST" \
     "schemingdcat.organization_custom_facets=$CKANEXT__SCHEMINGDCAT_ORGANIZATION_CUSTOM_FACETS" \
-    "schemingdcat.group_custom_facets=$CKANEXT__SCHEMINGDCAT_GROUP_CUSTOM_FACETS"
+    "schemingdcat.group_custom_facets=$CKANEXT__SCHEMINGDCAT_GROUP_CUSTOM_FACETS" \
+    "schemingdcat.geometadata_base_uri=$CKANEXT__SCHEMINGDCAT_GEOMETADATA_BASE_URI" \
+    "schemingdcat.default_package_item_icon"=$CKANEXT__SCHEMINGDCAT_DEFAULT_PACKAGE_ITEM_ICON" \
+    "schemingdcat.default_package_item_show_spatial"=$CKANEXT__SCHEMINGDCAT_DEFAULT_PACKAGE_ITEM_SHOW_SPATIAL"
 
-echo "ckanext-schemingdcat. Rebuild index"
+echo "[docker-entrypoint.02_setup_scheming] Rebuild index"
 ckan -c $CKAN_INI search-index rebuild
